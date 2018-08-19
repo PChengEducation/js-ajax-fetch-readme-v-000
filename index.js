1ebba20105cc3fe319ed276230b9ac78b340b134
Pan Cheng <pchengeducation@hotmail.com>
2018-08-19 01:59:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const app = "I don't do much.";
-
-fetch('https://api.github.com/user/repos').
-  then(res => res.json()).
-  then(json => console.log(json))
+const token = '3b99955360148f7991b2010bbf1d2efd1d1ed282'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
